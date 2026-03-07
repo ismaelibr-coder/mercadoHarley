60c80f85f3ade688eb23e943f05e55c5ba6b36c4
Ismael Borges Rodrigues <ismaelr@dbserver.com.br>
2026-03-06 22:23:25 -0300

    chore: FASE 1 - remover ~45 scripts obsoletos, pastas archive/ e firebase-export/

--- archive/obsolete-firebase/seedProducts.js
+++
@@ -1,5 +0,0 @@
-// Archived: seedProducts.js (originally seeded Firestore). Preserved for reference.
-// Use server-side migration scripts to move data to MySQL.
-// Original products data can be found in src/data/products.js
-
-console.log('seedProducts archived. Do not run in production.');
